2ae5f41441057c0651ee6b603d275a627745fbfc
mykha <efimenko.mykhailo@gmail.com>
2021-07-04 16:53:52 +0300

    Initial menu

---
+++ main.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :index
+end
+
+get '/about' do
+  @message = "There should be information about us"
+  erb :message
+  #erb "<div class=\"jumbotron text-center\"> About us information </div><h1></h1>"
+end
+
+get '/new' do
+  @message = "There should be NEW FORM"
+  erb :message
+end
